8b2dc45b7ec285bc37a7425a78f7d79ef91c77d1
Muhammed Faizal <mhdfaizalofficial@gmail.com>
2026-08-01 23:07:14 +0530

    disable bufferCommands on Mongoose to fail-fast on serverless cold starts in db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,6 +3,8 @@ const StorageService = require('../services/storageService');
 
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/behold_aspire';
 
+mongoose.set('bufferCommands', false);
+
 let isConnected = false;
 let dbPromise = null;
 
